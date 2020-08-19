b609dd2de07fe1c6d16c75d2b18020d682976bca
Denis Bystruev <dbystruev@me.com>
2020-08-19 19:18:17 +0300

    Created models

---
+++ Quiz 2020/Models/Answer.swift
@@ -0,0 +1,11 @@
+//
+//  Answer.swift
+//  Quiz 2020
+//
+//  Created by Denis Bystruev on 19.08.2020.
+//
+
+struct Answer {
+    let text: String
+    let type: AnimalType
+}
---
+++ Quiz 2020/Models/ResponseType.swift
@@ -0,0 +1,10 @@
+//
+//  ResponseType.swift
+//  Quiz 2020
+//
+//  Created by Denis Bystruev on 19.08.2020.
+//
+
+enum ResponseType {
+    case single, multiply, range
+}
